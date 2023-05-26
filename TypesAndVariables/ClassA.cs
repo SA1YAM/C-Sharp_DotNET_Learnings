672a77ccbdf81b50e7736665c0c113744957e78b
SA1YAM <121816424+SA1YAM@users.noreply.github.com>
2023-05-27 01:41:50 +0530

    Add files via upload

---
+++ TypesAndVariables/ClassA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace TypesAndVariables
+{
+    public class ClassA
+    {
+        public void Displayname() 
+        {
+            Console.WriteLine("My name is ClassA");
+        }
+    }
+}
